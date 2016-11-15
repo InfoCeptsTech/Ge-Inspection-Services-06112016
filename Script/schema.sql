f080b269d110ccf3e434d605d1eadc6ac9338228
sankalp-sontakke-InfoCepts <spsontakke@infocepts.com>
2016-11-15 13:39:50 +0530

    schema file updated as per current version
    schema file updated as per current version

--- Script/schema.sql
+++ Script/schema.sql
@@ -1,73 +1,85 @@
-CREATE TABLE users (
-  username VARCHAR(50) NOT NULL PRIMARY KEY,
-  email VARCHAR(50),
-  password VARCHAR(500),
-  activated BOOLEAN DEFAULT FALSE,
-  activationkey VARCHAR(50) DEFAULT NULL,
-  resetpasswordkey VARCHAR(50) DEFAULT NULL
-);
-
-CREATE TABLE authority (
-  name VARCHAR(50) NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE user_authority (
-    username VARCHAR(50) NOT NULL,
-    authority VARCHAR(50) NOT NULL,
-    FOREIGN KEY (username) REFERENCES users (username),
-    FOREIGN KEY (authority) REFERENCES authority (name),
-    UNIQUE INDEX user_authority_idx_1 (username, authority)
-);
-
-CREATE TABLE oauth_access_token (
-  token_id VARCHAR(256) DEFAULT NULL,
-  token BLOB,
-  authentication_id VARCHAR(256) DEFAULT NULL,
-  user_name VARCHAR(256) DEFAULT NULL,
-  client_id VARCHAR(256) DEFAULT NULL,
-  authentication BLOB,
-  refresh_token VARCHAR(256) DEFAULT NULL
-);
-
-CREATE TABLE oauth_refresh_token (
-  token_id VARCHAR(256) DEFAULT NULL,
-  token BLOB,
-  authentication BLOB
-);
-
-CREATE TABLE inspection_dtls
+CREATE TABLE authority
 (
-  blob_id character varying(50) NOT NULL,
-  asset_id character varying(255),
-  facility_id character varying(255),
-  inspection_id character varying(255),
-  inspection_phase_id character varying(255),
-  inspection_point_id character varying(255),
-  inspection_start timestamp with time zone,
-  inspection_stop timestamp with time zone,
-  inspection_type character varying(255),
-  inspector_id character varying(255),
-  required_field_of_view integer NOT NULL,
-  required_resolution integer NOT NULL,
-  sensor_name character varying(255),
-  sensor_sensor_mode character varying(255),
-  sensor_sensor_type character varying(255),
-  "timestamp" timestamp with time zone,
-  visibility character varying(255),
-  CONSTRAINT inspection_dtls_pkey PRIMARY KEY (blob_id)
+  name character varying(50) NOT NULL,
+  CONSTRAINT authority_pkey PRIMARY KEY (name)
 );
 
 CREATE TABLE inspection_media
 (
-  comment_id integer NOT NULL,
-  asset_id character varying(255),
-  blob_id character varying(255),
-  comment character varying(255),
-  inspection_date timestamp without time zone,
-  inspector_id character varying(255),
-  issue_date timestamp with time zone,
-  issue_id character varying(255),
-  issue_type character varying(255),
-  CONSTRAINT inspection_media_pkey PRIMARY KEY (comment_id)
+  id bigserial NOT NULL,
+  annotatedmetadata character varying(40000),
+  annotedcomments character varying(40000),
+  assetid character varying(255),
+  blobid character varying(255),
+  comment character varying(40000),
+  commentjson character varying(40000),
+  defecttype character varying(255),
+  description character varying(40000),
+  inspectiondate timestamp without time zone,
+  inspectionid character varying(255),
+  inspectorid character varying(255),
+  issueimage bytea,
+  statustype character varying(255),
+  "user" character varying(400),
+  inspection_requirement character varying(40000),
+  CONSTRAINT inspection_media_pkey1 PRIMARY KEY (id),
+  CONSTRAINT ukva22graa6xmesqil9hh2f0ft UNIQUE (blobid, "user")
 );
 
+CREATE TABLE inspection_report
+(
+  user_id character varying(255) NOT NULL,
+  comment character varying(400000),
+  CONSTRAINT inspection_report_pkey PRIMARY KEY (user_id)
+);
+
+CREATE TABLE inspectorpreferrence
+(
+  id character varying(255),
+  inspectorid character varying(255),
+  logo character varying(255),
+  approved_by character varying(255)
+);
+
+CREATE TABLE oauth_access_token
+(
+  token_id character varying(256) DEFAULT NULL::character varying,
+  token bytea,
+  authentication_id character varying(256) DEFAULT NULL::character varying,
+  user_name character varying(256) DEFAULT NULL::character varying,
+  client_id character varying(256) DEFAULT NULL::character varying,
+  authentication bytea,
+  refresh_token character varying(256) DEFAULT NULL::character varying
+);
+
+CREATE TABLE oauth_refresh_token
+(
+  token_id character varying(256) DEFAULT NULL::character varying,
+  token bytea,
+  authentication bytea
+);
+
+CREATE TABLE user_authority
+(
+  username character varying(50) NOT NULL,
+  authority character varying(50) NOT NULL,
+  CONSTRAINT user_authority_authority_fkey FOREIGN KEY (authority)
+      REFERENCES authority (name) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT user_authority_username_fkey FOREIGN KEY (username)
+      REFERENCES users (username) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT user_authority_username_authority_key UNIQUE (username, authority)
+);
+
+CREATE TABLE users
+(
+  username character varying(50) NOT NULL,
+  email character varying(50),
+  password character varying(500),
+  activated boolean DEFAULT false,
+  activationkey character varying(50) DEFAULT NULL::character varying,
+  resetpasswordkey character varying(50) DEFAULT NULL::character varying,
+  inspectorid character varying(50) DEFAULT NULL::character varying,
+  CONSTRAINT users_pkey PRIMARY KEY (username)
+);
